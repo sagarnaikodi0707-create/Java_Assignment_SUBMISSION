50fcc4fd622241557bcc9c7d6de6e318d4202aa4
naikodi-sagar <sagarnaikodi0707@gmail.com>
2026-04-19 13:26:46 +0530

    Added Question

--- src/main/java/eightB/CookieServlet.java
+++ src/main/java/eightB/CookieServlet.java
@@ -1,3 +1,6 @@
+/*8b. Build a servlet program to create a cookie to get your name through text box and press submit button( through HTML)
+to display the message by greeting Welcome back your name ! , you have visited this page n times ( n = number of your visit )
+along with the list of cookies and its setvalues and demonstrate the expiry of cookie also*/
 package eightB;
 
 import java.io.*;

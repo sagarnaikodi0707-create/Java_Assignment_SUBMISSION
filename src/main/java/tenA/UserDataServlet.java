be36538269117fbf3996b6d47a23d93fba2b9d78
naikodi-sagar <sagarnaikodi0707@gmail.com>
2026-04-19 14:36:24 +0530

    a

--- src/main/java/tenA/UserDataServlet.java
+++ src/main/java/tenA/UserDataServlet.java
@@ -1,3 +1,6 @@
+/*10.a	Build an Application to get Username,Email and Designation through JSP called index.jsp with client sided validation 
+and submit to the servlet called UserDataServlet and process all the fields with server sided validation and display all the data 
+through result.jsp with a link to move to the client side*/
 package tenA;
 
 import java.io.*;
